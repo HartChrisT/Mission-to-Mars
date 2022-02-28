702c6a5268bc445badbc3c6217bf968cf62cfc9e
HartChrisT <92996865+HartChrisT@users.noreply.github.com>
2022-02-27 19:16:10 -0500

    finished mars website

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -18,8 +19,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-
-.update_one(query_parameter, {"$set": data}, options)
 
 if __name__ == "__main__":
    app.run()
